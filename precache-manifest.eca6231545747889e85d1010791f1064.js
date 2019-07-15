4a2f8e5c87eb441cc6ef1249b82fe7ee653284b9
Sam Ceustermans <ceustermanssam1989@gmail.com>
2019-07-15 10:01:13 +0200

    Updates

---
+++ precache-manifest.eca6231545747889e85d1010791f1064.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cef9e8402eab9914fbd5809a4bbaba39",
+    "url": "/Robofriend/index.html"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/Robofriend/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "ae3f145137c21d20efa1",
+    "url": "/Robofriend/static/css/main.49e9442a.chunk.css"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/Robofriend/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "ae3f145137c21d20efa1",
+    "url": "/Robofriend/static/js/main.a6a72c2e.chunk.js"
+  },
+  {
+    "revision": "d5ac5f24d60e95a3cdb6",
+    "url": "/Robofriend/static/js/runtime~main.14978d9d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robofriend/static/media/SEGA.12a3db8b.woff"
+  }
+]);
